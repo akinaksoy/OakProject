6b9e514b4386a7edb31fdab97935f1cb66db3cb6
Akın Aksoy <akinaksoytr@gmail.com>
2023-05-10 19:21:38 +0300

    Initial Commit

---
+++ OakProject/OakProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OakProjectApp.swift
+//  OakProject
+//
+//  Created by Akın Aksoy on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct OakProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
